d51db48c01612112369c2aa16fa06c9c2ff804c0
Alex Johnston <me@alexj.ca>
2025-09-25 17:40:47 -0600

    Authentication bug

--- management_app/app/page.tsx
+++ management_app/app/page.tsx
@@ -2,17 +2,29 @@ import PageTitle from "@/components/page-title";
 import { auth0 } from "@/lib/auth0";
 import { cfg } from "@/lib/cfg";
 import TenantDataTable from "./tenant-data-table";
+import { redirect } from "next/navigation";
 
 
 export default async function Home() {
 
   const session = await auth0.getSession();
 
-  const items: { id: string, name: string, externalId?: string}[] = await fetch(`${cfg.apiBaseUrl}/tenant`, {
+  const res = await fetch(`${cfg.apiBaseUrl}/tenant`, {
     headers: {
       'Authorization': `Bearer ${session?.tokenSet.accessToken}`
     },
-  }).then(res => res.json());
+  });
+
+  if (res.status === 401) {
+    redirect('/auth/login');
+  }
+
+  if (!res.ok) {
+    console.log(res);
+    throw (await res.text())
+  }
+
+  const items: { id: string, name: string, externalId?: string}[] = await res.json();
 
   return (
     <div className="space-y-5">
