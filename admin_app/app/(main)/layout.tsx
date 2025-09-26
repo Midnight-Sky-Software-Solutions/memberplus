6f60b7701bb4ad0a6f6428a637dc578e627abe51
Alex Johnston <me@alexj.ca>
2025-09-26 15:20:04 -0600

    admin: MemberPlus logo

--- admin_app/app/(main)/layout.tsx
+++ admin_app/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import { Menubar } from "primereact/menubar"
 import { menuItems } from "./menu-items"
+import Image from "next/image"
 
 export default function DashboardLayout({
   children,
@@ -8,7 +9,12 @@ export default function DashboardLayout({
 }) {
   return (
     <>
-      <Menubar model={menuItems} end={<a className="p-button p-button-sm" href="/auth/logout">Logout</a>} />
+      <Menubar 
+        model={menuItems}
+        start={<Image width={120} height={12.8} src="/memberPLUS.svg" alt="MemberPlus logo" />}
+        end={<a className="p-button p-button-sm" 
+        href="/auth/logout">Logout</a>} 
+      />
       {children}
     </>
   ) 
