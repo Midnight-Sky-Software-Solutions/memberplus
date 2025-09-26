c1dfda53c008adfc433106172c1711f4175425b8
Alex Johnston <me@alexj.ca>
2025-09-26 17:00:20 -0600

    Clean up after ourselves

--- MemberPlus.Core/DatabaseProvider.cs
+++ MemberPlus.Core/DatabaseProvider.cs
@@ -18,6 +18,7 @@ namespace MemberPlus.Core
         public void CommitTransaction()
         {
             this.transaction!.Commit();
+            Connection.Close();
         }
 
         public IDbTransaction? Transaction => transaction;
