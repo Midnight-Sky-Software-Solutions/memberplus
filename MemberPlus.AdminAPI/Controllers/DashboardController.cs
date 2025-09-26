03731024a55fd5cbaee9b82a31c7b587a5c47a19
Alex Johnston <me@alexj.ca>
2025-09-26 04:56:58 -0600

    Create Admin API

---
+++ MemberPlus.AdminAPI/Controllers/DashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemberPlus.AdminAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+    }
+}
