502033ea531399268742220223c44f9901534873
Alex Johnston <me@alexj.ca>
2025-09-27 14:02:53 -0600

    admin: De-emphasize the logout button

--- admin_app/app/[accountId]/layout.tsx
+++ admin_app/app/[accountId]/layout.tsx
@@ -2,7 +2,7 @@
 import { Menubar } from "primereact/menubar"
 import { menuItems } from "./menu-items"
 import Image from "next/image"
-import apiClient, { ApiError } from "@/lib/api"
+import { ApiError } from "@/lib/api"
 import { redirect } from "next/navigation"
 import SideNav from "./side-nav"
 import Link from "next/link"
@@ -21,7 +21,7 @@ export default async function DashboardLayout({
         <Menubar 
           model={menuItems}
           start={<Link href="/"><Image width={120} height={12.8} src="/memberPLUS.svg" alt="MemberPlus logo" /></Link>}
-          end={<a className="p-button p-button-sm font-bold" 
+          end={<a className="p-button p-button-sm p-button-secondary font-bold" 
           href="/auth/logout">Logout</a>} 
         />
         <div className="grow flex">
