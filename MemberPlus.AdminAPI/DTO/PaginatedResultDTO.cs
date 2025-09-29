e0bc64ddbcbf3a5026fb525ccb599443c771cc5d
Alex Johnston <me@alexj.ca>
2025-09-29 04:49:49 -0600

    admin: Try updating swashbucke

--- MemberPlus.AdminAPI/DTO/PaginatedResultDTO.cs
+++ MemberPlus.AdminAPI/DTO/PaginatedResultDTO.cs
@@ -1,9 +1,13 @@
-﻿namespace MemberPlus.AdminAPI.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemberPlus.AdminAPI.DTO
 {
     public class PaginatedResultDTO<T>
     {
+        [Required]
         public int TotalRecords { get; set; }
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        [Required]
         public IEnumerable<T> Items { get; set; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     }
