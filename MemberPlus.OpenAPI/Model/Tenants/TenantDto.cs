ffb1413613f091faddb0776e7c868e9b8b2c0074
Alex Johnston <me@alexj.ca>
2025-10-20 07:31:58 -0600

    Show account name in menu bar

--- MemberPlus.OpenAPI/Model/Tenants/TenantDto.cs
+++ MemberPlus.OpenAPI/Model/Tenants/TenantDto.cs
@@ -1,15 +1,22 @@
-﻿namespace MemberPlus.OpenAPI.Model.Tenants
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemberPlus.OpenAPI.Model.Tenants
 {
     public record TenantDto
     {
+        [Required]
         public required Guid Id { get; init; }
+        [Required]
         public required string Name { get; init; }
+        [Required]
         public required IEnumerable<TenantAccountDto> Accounts { get; init; }
     }
 
     public record TenantAccountDto
     {
+        [Required]
         public required Guid Id { get; init; }
+        [Required]
         public required string Name { get; init; }
     }
 }
--- tenant_app/app/routes/authenticated/layout.tsx
+++ tenant_app/app/routes/authenticated/layout.tsx
@@ -1,5 +1,8 @@
 import apiClient from "lib/api";
-import { Outlet, redirect } from "react-router";
+import { Link, Outlet, redirect } from "react-router";
+import type { components } from "lib/api.d";
+import { Menubar } from "primereact/menubar";
+import type { MenuItem } from "primereact/menuitem";
 
 export async function clientLoader() {
   const { data, error, response } = await apiClient.GET("/api/Tenants/me");
@@ -13,15 +16,23 @@ export async function clientLoader() {
 
 export default function AuthenticatedLayout({ loaderData }: {
   loaderData: {
-    tenant: {
-      id: string,
-      name: string
-    }
+    tenant: components["schemas"]["TenantDto"]
   }
 }) {
+  const account = loaderData.tenant.accounts[0];
+  const menuItems: MenuItem[] = [
+  ];
+
   return (
     <div>
-      <Outlet />
+      <Menubar 
+        start={<Link to='/' className="font-bold text-2xl">MemberPlus</Link>}
+        model={menuItems}
+        end={<span>{account.name}</span>}
+      />
+      <div className="p-10">
+        <Outlet />
+      </div>
     </div>
   );
 }
