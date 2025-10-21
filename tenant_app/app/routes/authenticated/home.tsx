57be2e8572511d6d3722c029f72188cd67b89292
Alex Johnston <me@alexj.ca>
2025-10-21 07:16:51 -0600

    TabMenu tabs

--- tenant_app/app/routes/authenticated/home.tsx
+++ tenant_app/app/routes/authenticated/home.tsx
@@ -1,7 +1,6 @@
-import { AccountContext } from "context/account-context"
 import type { MenuItem } from "primereact/menuitem";
 import { TabMenu } from "primereact/tabmenu";
-import { useContext } from "react"
+import { useState } from "react"
 
 const tabMenuItems: MenuItem[] = [
   {
@@ -19,14 +18,49 @@ const tabMenuItems: MenuItem[] = [
 ];
 
 export default function Home() {
-  const account = useContext(AccountContext);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <>
-      <TabMenu className="w-full" model={tabMenuItems} />
+      <TabMenu 
+        className="w-full" 
+        model={tabMenuItems}
+        onTabChange={e => setActiveIndex(e.index)}
+        activeIndex={activeIndex}
+      />
+      <TabMenuOutlet activeIndex={activeIndex} />
+    </>
+  )
+}
+
+function TabMenuOutlet({ activeIndex }: {
+  activeIndex: number
+}) {
+
+  if (activeIndex === 0) {
+    return (
       <div className="p-10">
         <h1 className="font-bold text-4xl">Dashboard</h1>
       </div>
-    </>
-  )
+    );
+  }
+
+  if (activeIndex === 1) {
+    return (
+      <div className="p-10">
+        <h1 className="font-bold text-4xl">Account details</h1>
+      </div>
+    );
+  }
+
+  if (activeIndex === 2) {
+    return (
+      <div className="p-10">
+        <h1 className="font-bold text-4xl">Referrals</h1>
+      </div>
+    );
+  }
+
+  return <></>;
+
 }
