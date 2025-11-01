326a261fe82691132d378d4291182cabfe176082
Alex Johnston <me@alexj.ca>
2025-11-01 05:25:57 -0600

    Cancel subscription when cancelling membership

--- MemberPlus.Schema/dbo/Stored Procedures/Contact/sp_CancelMembership.sql
+++ MemberPlus.Schema/dbo/Stored Procedures/Contact/sp_CancelMembership.sql
@@ -4,6 +4,20 @@
 AS
 BEGIN
 
+BEGIN TRANSACTION
+
+UPDATE dbo.Subscription
+SET
+    IsActive = 0
+WHERE
+    ContactId = @ContactId
+    AND ContactId IN (
+        SELECT Id
+        FROM dbo.Contact
+        WHERE
+            AccountId = @AccountId
+    )
+
 UPDATE dbo.Contact
 SET
     MemberStatusId = (SELECT Id FROM dbo.MemberStatus WHERE [Code] = 'MS05')
@@ -11,5 +25,7 @@ WHERE
     ID = @ContactId
     AND AccountId = @AccountId
     AND MemberStatusId = (SELECT Id FROM dbo.MemberStatus WHERE [Code] = 'MS02');
+
+COMMIT TRANSACTION
 
 END
